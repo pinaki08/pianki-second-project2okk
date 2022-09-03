ac480b10e8ec71d0bfb1ea4f9a6d72b4fa0bed78
pinakighosh94 <pinakiranjanghosh2022@gmail.com>
2022-09-03 22:55:10 +0530

    bootstraps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import Header from "./Component/Header";
+import Grid from "./Component/Gird";
+import "./App.css";
 import AreaOfRectangle from "./Component/AreaOfRectangle";
 import { AreaOfcircle } from "./Component/AreaOfcircle";
+import { Container } from "react-bootstrap";
+
 function App() {
   return (
-    <div>
-      <AreaOfRectangle />
-      <AreaOfcircle />
+    <div className="amrcontainer">
+      <Header />
+
+      <Container>
+        {/*
+        <Row>
+          <Col>
+            <Grid />
+          </Col>
+          <Col>2 of 2</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+        */}
+        <Grid />
+        <AreaOfRectangle />
+        <AreaOfcircle />
+      </Container>
     </div>
   );
 }
